a85162ff7f735a67a46431bbb42e0975d0c5f243
Alexandr Biktimirov <alex.biktimirov38@gmail.com>
2025-11-26 15:24:53 +0100

    feat: implement adapter design pattern

--- DesignPattern/Adapter/EmployeeAdapter.cs
+++ DesignPattern/Adapter/EmployeeAdapter.cs
@@ -1,13 +1,24 @@
-﻿// In case you need some guidance: https://refactoring.guru/design-patterns/adapter
-namespace DesignPattern.Adapter
+﻿namespace DesignPattern.Adapter
 {
     public class EmployeeAdapter : ITarget
     {
         private readonly BillingSystem thirdPartyBillingSystem = new();
 
-        //public void ProcessCompanySalary(...)
-        //{
-        //    ...
-        //}
+        public void ProcessCompanySalary(string[,] employeesArray)
+        {
+            List<Employee> employees = [];
+
+            for (var i = 0; i < employeesArray.GetLength(0); i++)
+            {
+                var id = int.Parse(employeesArray[i, 0]);
+                var name = employeesArray[i, 1];
+                var designation = employeesArray[i, 2];
+                var salary = decimal.Parse(employeesArray[i, 3]);
+                
+                employees.Add(new Employee(id, name, designation, salary));
+            }
+
+            thirdPartyBillingSystem.ProcessSalary(employees);
+        }
     }
 }
